9f9d840f0d05afc66043bcf7f97163177f24699d
NandhithaaMP <nandhitha2122@gmail.com>
2025-03-05 22:07:17 +0530

    qibla commit

---
+++ lib/screens/test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TestScreen extends StatelessWidget {
+  const TestScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text("Qibla finder"),
+        ],
+      ),
+    );
+  }
+}
